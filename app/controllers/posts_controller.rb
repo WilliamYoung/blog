42a1946c7b63c9eacef385108ce5da95396c81e8
WilliamYoung <evaflamen@hotmail.com>
2015-02-26 20:28:03 -0800

    Added posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all.order('created_at DESC')			
+	end #index
+
+	def new	
+		
+	end #new
+
+	def create
+		@post = Post.new post_params
+
+		@post.save
+		redirect_to @post
+	end #create
+
+	def show
+		@post = Post.find params[:id]
+	end #show
+
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end #post_params
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,13 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+
+  Rails.application.routes.draw do
+    resources :posts
+    root "posts#index"
+  end
+
+
   # Example resource route with options:
   #   resources :products do
   #     member do
