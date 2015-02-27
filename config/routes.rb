3fc78c46252810011191b978b96afd0ff34e93b9
WilliamYoung <evaflamen@hotmail.com>
2015-02-27 00:49:35 -0800

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
 
 
   Rails.application.routes.draw do
-    resources :posts
+    resources :posts do
+      resources :comments
+    end
     root "posts#index"
   end
 
